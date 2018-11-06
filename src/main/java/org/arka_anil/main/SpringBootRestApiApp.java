54868280aab872c3ad72ed2a6e6f99dca6b612cd
Arka Dutta <arkadutta@VER-BLR-LT1722.local>
2018-11-06 09:00:50 +0530

    Added Hibernate : JPA : Skeleton

--- src/main/java/org/arka_anil/main/SpringBootRestApiApp.java
+++ src/main/java/org/arka_anil/main/SpringBootRestApiApp.java
@@ -2,8 +2,12 @@ package org.arka_anil.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages={"org.arka_anil"})
+@SpringBootApplication(scanBasePackages = {"org.arka_anil"})
+@EnableJpaRepositories(basePackages = {"org.arka_anil"})
+@EntityScan(basePackages = {"org.arka_anil"})
 public class SpringBootRestApiApp {
 
   public static void main(String[] args) {
---
+++ src/main/java/org/arka_anil/model/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package org.arka_anil.model;
+
+public class StudentNotFoundException extends RuntimeException {
+
+  public StudentNotFoundException(String exception) {
+    super(exception);
+  }
+
+}
